88e35fb595ac743ca5199aabe98ee7c395a053a7
José Hidasi-Neto <hidasineto@gmail.com>
2022-07-07 18:12:26 -0300

    menu updated

--- Assets/Scripts/mainmenu.cs
+++ Assets/Scripts/mainmenu.cs
@@ -4,16 +4,70 @@ using UnityEngine;
 
 public class mainmenu : MonoBehaviour
 {
-    public GameObject lightning1,lightning2,image1,image2;
+    public GameObject lightning1,lightning2,image1,image2, A, W, S;
+    public float timerfull = 4f, timerfullactive=1;
+    public float timer,timeractive;
+    private bool calc;
+    private int rand=3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = timerfull;
+        timeractive = timerfullactive;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (calc == false)
+            {
+                rand = Random.Range(0, 3);
+                calc = true;
+            }
+            if (rand == 0)
+            {
+                A.SetActive(true);
+                timeractive -= Time.deltaTime;
+                if (timeractive <= 0)
+                {
+                    A.SetActive(false);
+                    rand = 3;
+                    timeractive = timerfullactive;
+                    timer = timerfull;
+                    calc = false;
+                }
+            }
+            if (rand == 1)
+            {
+                W.SetActive(true);
+                timeractive -= Time.deltaTime;
+                if (timeractive <= 0)
+                {
+                    W.SetActive(false);
+                    rand = 3;
+                    timeractive = timerfullactive;
+                    timer = timerfull;
+                    calc = false;
+                }
+            }
+            if (rand == 2)
+            {
+                S.SetActive(true);
+                timeractive -= Time.deltaTime;
+                if (timeractive <= 0)
+                {
+                    S.SetActive(false);
+                    rand = 3;
+                    timeractive = timerfullactive;
+                    timer = timerfull;
+                    calc = false;
+                }
+            }
+        }
+
         float direction = Input.GetAxisRaw("Vertical") * Time.deltaTime;
         if (direction > 0)
         {
